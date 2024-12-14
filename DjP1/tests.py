37ca960c255074924b6dceb3e73b537d166b1351
Farzan-79 <far.kalantarii@gmail.com>
2024-12-14 18:19:29 +0330

    2- added test + some mod

---
+++ DjP1/tests.py
@@ -0,0 +1,19 @@
+import os
+from django.conf import settings
+from django.contrib.auth.password_validation import validate_password
+from django.test import TestCase
+from django.core.exceptions import ValidationError, ImproperlyConfigured
+
+class DjP1Test (TestCase):
+    def test_secret_key(self):
+        try:
+            validate_password(settings.SECRET_KEY)
+        except ValidationError as e:
+            msg = f'Weak Secret Key: {e.messages}'
+            self.fail(msg)
+        except ImproperlyConfigured as e:
+            msg = 'no secret key found'
+            self.fail(msg)
+        self.assertNotEqual(settings.SECRET_KEY, 'fkn4850217184', 'you cannot have 0217184 as the secret key you dumbass')
+
+    
